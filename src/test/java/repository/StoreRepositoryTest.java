f2072b48a7981b32acc51156f9040b330893f5a9
Unknown <suciuirina97@gmail.com>
2018-03-31 20:25:23 +0300

    Test added in the laboratory changed

--- src/test/java/repository/StoreRepositoryTest.java
+++ src/test/java/repository/StoreRepositoryTest.java
@@ -8,12 +8,12 @@ import static junit.framework.TestCase.assertTrue;
 /**
  * Created by dell on 3/27/2018.
  */
-class StoreRepositoryTest {
+public class StoreRepositoryTest {
 
     private StoreRepository repository=new StoreRepository();
 
     @Test
-    void addNewProduct1() {
+    public void addNewProduct1() {
         Product product=new Product(100,"ananas","fructe",89);
         try {
             assertTrue(repository.addNewProduct(product));
@@ -24,12 +24,12 @@ class StoreRepositoryTest {
     }
 
     @Test
-    void addNewProduct2() {
+    public void addNewProduct2() {
         Product product=new Product(-100,"ananas","fructe",-67);
         try {
             assertTrue(repository.addNewProduct(product));
         } catch (Exception e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
 
     }
