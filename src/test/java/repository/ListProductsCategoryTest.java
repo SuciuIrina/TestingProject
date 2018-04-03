0bb4953db70e565eea9457f6250a2ffcfd8e219c
Unknown <suciuirina97@gmail.com>
2018-04-03 22:46:10 +0300

    One method created for Lab3

---
+++ src/test/java/repository/ListProductsCategoryTest.java
@@ -0,0 +1,30 @@
+package repository;
+
+import model.Product;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dell on 4/3/2018.
+ */
+public class ListProductsCategoryTest {
+
+    private StoreRepository repo;
+
+    public ListProductsCategoryTest() throws IOException {
+        this.repo = new StoreRepository("products.txt");
+    }
+
+    @Test
+    public void getProductsFromCategory(){
+        String category="fructe";
+        ArrayList<Product> result=new ArrayList<Product>();
+        result=repo.getProductsCategory(category);
+
+        assertEquals(1,result.size());
+    }
+}
